274691b4636061cb9035d887a56c5c7508b5bb69
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-30 18:03:02 +0600

    module 34.4 done

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ const loadPhones = async (phoneBrand) => {
 const displayPhones = (phoneData) => {
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.innerHTML = ``;
+    // Show Only 10 Phones
+    phoneData = phoneData.slice(0, 10);
+    // Show if no phone is found
+    const noPhoneFound = document.getElementById("no-phone-found");
+    if (phoneData.length === 0) {
+        noPhoneFound.classList.remove("d-none");
+        // stop spinner
+        toggleSpinner(false);
+    }
+    else {
+        noPhoneFound.classList.add("d-none");
+    }
     for (const phone of phoneData) {
         const phoneDiv = document.createElement("div");
         phoneDiv.classList.add("col");
@@ -21,16 +33,34 @@ const displayPhones = (phoneData) => {
         </div>
          `
         phoneContainer.appendChild(phoneDiv);
+        // stop spinner
+        toggleSpinner(false);
     }
+
+
 
 
 }
 
 document.getElementById("btn-search").addEventListener("click", function () {
+    // Start Spinner
+    toggleSpinner(true);
+    // Collect User Input Value
     const inputSearchField = document.getElementById("input-search");
     const inputSearchValue = inputSearchField.value;
     loadPhones(inputSearchValue);
 
 })
+
+// Show Spinner Function
+const toggleSpinner = (isLoading) => {
+    const spinnerDiv = document.getElementById("spinner");
+    if (isLoading) {
+        spinnerDiv.classList.remove("d-none");
+    }
+    else {
+        spinnerDiv.classList.add("d-none");
+    }
+}
 
 loadPhones("a");

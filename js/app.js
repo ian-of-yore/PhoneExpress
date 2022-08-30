dc1052cd908953d39527cd44aeb23ea9ddea6ba3
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-30 18:36:42 +0600

    module 34.5 done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-const loadPhones = async (phoneBrand) => {
+const loadPhones = async (phoneBrand, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneBrand}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data)
+    displayPhones(data.data, dataLimit)
 }
 
-const displayPhones = (phoneData) => {
+
+const displayPhones = (phoneData, dataLimit) => {
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.innerHTML = ``;
-    // Show Only 10 Phones
-    phoneData = phoneData.slice(0, 10);
-    // Show if no phone is found
+
+    // Show All button and Data Limit
+    const showAll = document.getElementById("show-all");
+    if (dataLimit && phoneData.length > 10) {
+        phoneData = phoneData.slice(0, 10);
+        showAll.classList.remove("d-none");
+    }
+    else {
+        showAll.classList.add("d-none");
+    }
+
+
+
+
     const noPhoneFound = document.getElementById("no-phone-found");
     if (phoneData.length === 0) {
         noPhoneFound.classList.remove("d-none");
-        // stop spinner
         toggleSpinner(false);
     }
     else {
@@ -36,21 +47,30 @@ const displayPhones = (phoneData) => {
         // stop spinner
         toggleSpinner(false);
     }
-
-
-
-
 }
 
-document.getElementById("btn-search").addEventListener("click", function () {
+// Function to process Data
+const processData = (dataLimit) => {
     // Start Spinner
     toggleSpinner(true);
     // Collect User Input Value
     const inputSearchField = document.getElementById("input-search");
     const inputSearchValue = inputSearchField.value;
-    loadPhones(inputSearchValue);
+    loadPhones(inputSearchValue, dataLimit);
+}
 
+
+
+// Search Button Event Handler
+document.getElementById("btn-search").addEventListener("click", function () {
+    processData(10);
 })
+
+// Show All Button Event Handler
+document.getElementById("btn-show-all").addEventListener("click", function () {
+    processData();
+})
+
 
 // Show Spinner Function
 const toggleSpinner = (isLoading) => {
